c3fe642f117d744d89853874cc5c76f547f0625b
ruida shen <shenruida@gmail.com>
2020-08-01 23:17:16 -0700

    Update topnav styles

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div>
-    Nav Bar |
-    <router-link to="/">Ruida</router-link>|
-    <router-link to="/doc">Ruida 2</router-link>
     <router-view></router-view>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+      <div class="logo">Logo</div>
+      <ul class="menu">
+        <li>item 1</li>
+        <li>item 2</li>
+      </ul>
     </div>
     <div class="banner">
       <h1>Rei UI</h1>
@@ -14,5 +17,26 @@
     </div>
   </div>
 </template>
+
 <style lang="scss" scoped>
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
 </style>
