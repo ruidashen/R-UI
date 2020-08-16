aba9500188cc67fc548cc59a7343d1f460c21ba9
ruida shen <shenruida@gmail.com>
2020-08-15 21:44:46 -0700

    finished button components and doc

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,3 +1,72 @@
 <template>
   <div>Button Demo</div>
+  <h1>demo 1</h1>
+  <div>
+    <Button>Button</Button>
+    <Button theme="button">Button</Button>
+    <Button theme="link">Button</Button>
+    <Button theme="text">Button</Button>
+  </div>
+
+  <h1>demo 2</h1>
+  <div>
+    <Button size="big">Big Button</Button>
+    <Button size="normal">normal</Button>
+    <Button size="small">sm</Button>
+    <br />
+    <br />
+    <Button size="big" theme="link">Big Button</Button>
+    <Button size="normal" theme="link">normal</Button>
+    <Button size="small" theme="link">sm</Button>
+    <br />
+    <br />
+    <Button size="big" theme="text">Big Button</Button>
+    <Button size="normal" theme="text">normal</Button>
+    <Button size="small" theme="text">sm</Button>
+  </div>
+
+  <h1>demo 3</h1>
+  <div>
+    <Button>Normal</Button>
+    <Button level="main">Main</Button>
+    <Button level="danger">Danger</Button>
+    <br />
+    <br />
+    <Button theme="link">Normal</Button>
+    <Button theme="link" level="main">Main Link</Button>
+    <Button theme="link" level="danger">Danger Link</Button>
+    <br />
+    <br />
+    <Button theme="text">Normal</Button>
+    <Button theme="text" level="main">Main Text</Button>
+    <Button theme="text" level="danger">Danger Text</Button>
+  </div>
+
+  <h1>demo 4</h1>
+  <div>
+    <Button disabled>Disabled</Button>
+    <Button :disabled="true">Disabled</Button>
+    <br />
+    <br />
+    <Button theme="link" level="main" disabled>Main Link</Button>
+    <Button theme="link" level="danger" disabled>Danger Link</Button>
+    <br />
+    <br />
+    <Button theme="text" level="main" disabled>Main Text</Button>
+    <Button theme="text" level="danger" disabled>Danger Text</Button>
+  </div>
+  <h1>demo 5</h1>
+  <div>
+    <Button loading>Loading</Button>
+    <Button>Loaded</Button>
+  </div>
 </template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,199 @@
+<template>
+  <button class="rui-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="rui-loadingIndicator"></span>
+    <slot />
+  </button>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: "button",
+    },
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      return [`rui-theme-${theme}`, `rui-size-${size}`, `rui-level-${level}`];
+    });
+    return { classes };
+  },
+};
+</script>
+
+<style lang="scss">
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+$red: red;
+$grey: grey;
+.rui-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+  &.rui-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.rui-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+
+  &.rui-theme-button {
+    &.rui-size-big {
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px;
+    }
+
+    &.rui-size-small {
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
+
+    &.rui-level-main {
+      background: darken($blue, 10%);
+      color: white;
+      border-color: darken($blue, 10%);
+
+      &:hover,
+      &:focus {
+        background: $blue;
+        border-color: $blue;
+      }
+    }
+
+    &.rui-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+
+  &.rui-theme-link {
+    &.rui-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.rui-theme-text {
+    &.rui-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.rui-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.rui-theme-link,
+  &.rui-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+
+  > .rui-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: rui-spin 1s infinite linear;
+  }
+}
+@keyframes rui-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
