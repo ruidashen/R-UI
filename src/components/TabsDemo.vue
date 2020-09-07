d9fbb30928241cdbf81027b55de8a610457efbe0
ruida shen <shenruida@gmail.com>
2020-09-06 23:23:24 -0700

    Finished Tabs component

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,3 +1,24 @@
 <template>
   <div>Tabs Demo</div>
-</template>
+  <h1>Demo 1</h1>
+  <Tabs v-model:selected="x">
+    <Tab title="nav1">Content 1</Tab>
+    <Tab title="nav22222222222222222">Content 2</Tab>
+  </Tabs>
+</template>2
+
+<script>
+import Tab from "../lib/Tab.vue";
+import Tabs from "../lib/Tabs.vue";
+import { ref } from "vue";
+export default {
+  components: {
+    Tabs,
+    Tab,
+  },
+  setup() {
+    const x = ref("nav1");
+    return { x };
+  },
+};
+</script>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="rui-tabs">
+    <div class="rui-tabs-nav" ref="container">
+      <div
+        class="rui-tabs-nav-item"
+        v-for="(t,index) in titles"
+        :key="index"
+        :class="{selected : t===selected}"
+        @click="select(t)"
+        :ref="el => {if (t===selected) selectedItem = el}"
+      >{{t}}</div>
+      <div class="rui-tabs-nav-indicator" ref="indicator"></div>
+    </div>
+    <div class="rui-tabs-content">
+      <component class="rui-tabs-content-item" :is="current" :key="current.props.title" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Tab from "./Tab.vue";
+import { ref, watchEffect, onMounted, computed } from "vue";
+
+export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
+  setup(props, context) {
+    const selectedItem = ref<HTMLDivElement>(null);
+    const defaults = context.slots.default();
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+
+    onMounted(() => {
+      watchEffect(() => {
+        const { width } = selectedItem.value.getBoundingClientRect();
+        indicator.value.style.width = width + "px";
+        const { left: left1 } = container.value.getBoundingClientRect();
+        const { left: left2 } = selectedItem.value.getBoundingClientRect();
+        const left = left2 - left1;
+        indicator.value.style.left = left + "px";
+      });
+    });
+
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("<Tabs> can only contain <Tab> subcomponents");
+      }
+    });
+
+    const current = computed(() => {
+      return defaults.find((tag) => {
+        return tag.props.title === props.selected;
+      });
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    const select = (title: string) => {
+      context.emit("update:selected", title);
+    };
+    return {
+      defaults,
+      titles,
+      current,
+      select,
+      indicator,
+      container,
+      selectedItem,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.rui-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background-color: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
